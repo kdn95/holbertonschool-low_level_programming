992cb2e762a4353530ecc093e5e84906ac9aa476
Khang <khangdn95@gmail.com>
2024-02-22 04:37:56 +0000

    updated doc style

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/**
+ * main - print a string with newline
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
