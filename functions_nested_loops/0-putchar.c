3381265141ca2f9588d7494130c31629f6a7dd94
Khang <khangdn95@gmail.com>
2024-02-28 02:56:05 +0000

    updated 0 with _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * main - print _putchar
  *
@@ -6,13 +7,14 @@
  */
 int main(void)
 {
-const char *str = "_putchar";
-int i = 0;
-while (str[i] != '\0')
-{
-putchar(str[i]);
-i++;
-}
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
