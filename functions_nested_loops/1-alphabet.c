31943c60109c8c92d434be25dbc64c8ab868ef1e
Khang <khangdn95@gmail.com>
2024-02-28 05:59:04 +0000

    added 2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
+#include"main.h"
 /**
- * main - print alphabet
+ * print_alphabet - print in lwrcase
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-  
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
