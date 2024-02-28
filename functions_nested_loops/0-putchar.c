38c8b7854bac6ebca227b59c3c6995b6645ae8b7
Khang <khangdn95@gmail.com>
2024-02-28 04:02:31 +0000

    updated 0#4

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 /**
  * main - print _putchar
@@ -7,12 +6,14 @@
  */
 int main(void)
 {
-char *c = "_putchar";
-while (*c)
-{
-putchar(*c);
-c++;
-}
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+  
