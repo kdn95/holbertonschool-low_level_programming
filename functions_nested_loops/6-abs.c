4be208fa817a7055dc3e7d0267426aaf6db4fc50
Khang <khangdn95@gmail.com>
2024-03-03 03:42:14 +0000

    updated 6 & 7

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - find abs value of integer
+ * @n: the value to check
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
