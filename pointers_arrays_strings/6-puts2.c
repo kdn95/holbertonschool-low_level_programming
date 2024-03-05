429d89ab3c05590ca2a9dd09b1639e7a2856c950
Khang <khangdn95@gmail.com>
2024-03-05 09:10:02 +0000

    updated 7

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print every other character
+ * @str: string
+ */
+void puts2(char *str)
+{
+int l = 0;
+while (str[l] != '\0')
+{
+_putchar(str[l]);
+l = l + 2;
+}
+_putchar('\n');
+}
