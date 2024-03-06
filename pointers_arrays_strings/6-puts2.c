3fbadcbee0adcdfbed2872c2456e69788d2181ca
Khang <khangdn95@gmail.com>
2024-03-06 23:14:37 +0000

    updated 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,18 +4,12 @@
  * @str: string
  */
 void puts2(char *str)
-{
-int l6, i6;
-l6 = 0;
-i6 = 0;
-while (str[l6] != '\0')
-{
-l6++;
-}
-while(i6 < l6)
-{
-_putchar(str[l6]);
-l6 = l6 + 2;
-}
+{	
+int i;	
+for (i = 0; str[i] != '\0' ; i++)	
+{		
+if ((i % 2) == 0)			
+_putchar(*(str + i));	
+}	
 _putchar('\n');
 }
