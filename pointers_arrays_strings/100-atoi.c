2ae8ba634eb47eb61d04df6825c0d6650883ba16
Khang <khangdn95@gmail.com>
2024-03-06 07:02:28 +0000

    added 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,13 +6,33 @@
  */
 int _atoi(char *s)
 {
-  int l, i, a, d, e = 0;
+int l, i, na, m, nb, num;
+l = 0;
+i = 0;
+na = 0;
+m = 0;
+nb = 0;
+num = 0;
 while (s[l] != '\0')
-{
 l++;
-}
-while (i < l && a == 0)
+while (i < l && na == 0)
 {
 if (s[i] == '-')
-++d;
-
+++m;
+if (s[i] >= '0' && s[i] <= '9')
+{
+nb = s[i] - '0';
+if (m % 2)
+nb = -nb;
+num = num * 10 + nb;
+na = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+na = 0;
+}
+i++;
+}
+if (na == 0)
+return (0);
+return (num);
+}
