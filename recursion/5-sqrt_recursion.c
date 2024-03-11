8e7ba21ae8bee1c27ef67dd7ddd735ca9881e1e7
Khang <khangdn95@gmail.com>
2024-03-11 23:30:06 +0000

    updated 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "main.h"
 /**
+ * _sqrt_finder - sqroot
+ * finder to check root number
+ * @n: number
+ * @rootn: root number
+ * Return: rootn -1 if
+ * no natural sqroot
+ */
+int _sqrt_finder(int n, int rootn)
+{
+if (rootn * rootn == n)
+{
+return (rootn);
+}
+else if (rootn * rootn > n)
+{
+return (-1);
+}
+else
+return (_sqrt_finder(n, rootn + 1));
+}
+/**
  * _sqrt_recursion - sqroot
  * of given number
  * @n: number
@@ -8,8 +29,13 @@
  */
 int _sqrt_recursion(int n)
 {
-  if (((n * _sqrt_recursion(n ,2) / 2) % 10
-!= 0))
+int rootn = 0;
+if (n < 0)
 {
-  return (-1);
- }
+return (-1);
+}
+else
+{
+return (_sqrt_finder(n, rootn));
+}
+}
