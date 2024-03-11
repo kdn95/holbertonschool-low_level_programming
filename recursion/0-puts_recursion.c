5f15f8442301d3c86860c51cbfe74adec3417498
Khang <khangdn95@gmail.com>
2024-03-11 06:15:03 +0000

    updated 0-1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-int i;
-i = 0;
-while (s[i] != '\0')
+if (*s == 0)
 {
-_putchar(s[i]);
-i++;
-}
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,15 +5,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-int i, j;
-i = 0;
-while (s[i] != '\0')
-i++;
-j = i - 1;
-while (j >= 0)
+if (*s == '\0')
 {
-_putchar(s[j]);
-j--;
+return;
 }
-_putchar('\n');
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
