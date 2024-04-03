f06a956246d2a7f72015c05e13d9255546655457
Khang <khangdn95@gmail.com>
2024-04-03 01:03:14 +0000

    added 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * print_dlistint - print all elements
+ * @h: pointer to dlistint_t
+ * Return: return count
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t size = 0;
-  
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
