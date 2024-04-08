b11ad2b0f21677551dda5a94e33983230e5b247a
Khang <khangdn95@gmail.com>
2024-04-08 12:55:18 +0000

    added 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - set value of bit to 1 at given ind
+ * @n: pointer to UL
+ * @index: given ind
+ * Return: 1 for success and -1 for fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int x = 1 << index;
+if (n == NULL)
+{
+return (-1);
+}
+(*n) |= x;
+return (1);
+}
